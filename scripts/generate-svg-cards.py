fd6439aa8734b4eddd2b44e17d831a0619cb1163
WJH <3506456886@qq.com>
2026-01-25 19:31:46 +0800

    feat: creations 4 Bilibili

--- scripts/generate-svg-cards.py
+++ scripts/generate-svg-cards.py
@@ -16,11 +16,11 @@ def format_number(num):
 
 def generate_blog_card(stats):
     """生成Blog统计SVG卡片 - Tokyo Night主题（与.ref样式一致）"""
-    fans = format_number(stats.get('fans', 0))
+    followers = format_number(stats.get('fans', 0))
     likes = format_number(stats.get('likes', 0))
-    collect = format_number(stats.get('collect', 0))
-    original = format_number(stats.get('original', 0))
     views = format_number(stats.get('views', 0))
+    creations = format_number(stats.get('original', 0))
+    stars = format_number(stats.get('collect', 0))
 
     # 340px 宽度与 Stats 卡片一致，README 中通过 width="32%" 控制显示大小
     width = 340
@@ -47,7 +47,7 @@ def generate_blog_card(stats):
             </g>
             <g transform="translate(0,50.4)" width="14" height="14" fill="#bf91f3">
                 <path fill-rule="evenodd"
-                    d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z" />
+                    d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z" />
             </g>
             <g transform="translate(0,75.6)" width="14" height="14" fill="#bf91f3">
                 <path fill-rule="evenodd"
@@ -55,17 +55,17 @@ def generate_blog_card(stats):
             </g>
             <g transform="translate(0,100.8)" width="14" height="14" fill="#bf91f3">
                 <path fill-rule="evenodd"
-                    d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z" />
-            </g><text x="21" y="14" style="fill: #38bdae; font-size: 14px;">followers:</text><text x="21" y="39.2"
+                    d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z" />
+            </g><text x="21" y="14" style="fill: #38bdae; font-size: 14px;">Followers:</text><text x="21" y="39.2"
                 style="fill: #38bdae; font-size: 14px;">Likes:</text><text x="21" y="64.4"
-                style="fill: #38bdae; font-size: 14px;">Stars:</text><text x="21" y="89.6"
-                style="fill: #38bdae; font-size: 14px;">Creation:</text><text x="21" y="114.8"
-                style="fill: #38bdae; font-size: 14px;">Views:</text><text x="100" y="14"
-                style="fill: #38bdae; font-size: 14px;">{fans}</text><text x="100" y="39.2"
+                style="fill: #38bdae; font-size: 14px;">Views:</text><text x="21" y="89.6"
+                style="fill: #38bdae; font-size: 14px;">Creations:</text><text x="21" y="114.8"
+                style="fill: #38bdae; font-size: 14px;">Stars:</text><text x="100" y="14"
+                style="fill: #38bdae; font-size: 14px;">{followers}</text><text x="100" y="39.2"
                 style="fill: #38bdae; font-size: 14px;">{likes}</text><text x="100" y="64.4"
-                style="fill: #38bdae; font-size: 14px;">{collect}</text><text x="100" y="89.6"
-                style="fill: #38bdae; font-size: 14px;">{original}</text><text x="100" y="114.8"
-                style="fill: #38bdae; font-size: 14px;">{views}</text>
+                style="fill: #38bdae; font-size: 14px;">{views}</text><text x="100" y="89.6"
+                style="fill: #38bdae; font-size: 14px;">{creations}</text><text x="100" y="114.8"
+                style="fill: #38bdae; font-size: 14px;">{stars}</text>
         </g>
         <g transform="translate(220,20)">
             <g transform="scale(6)" style="fill: #bf91f3;">
@@ -83,10 +83,11 @@ def generate_blog_card(stats):
 def generate_vlog_card(stats):
     """生成Vlog统计SVG卡片 - Tokyo Night主题（与.ref样式一致）"""
     followers = format_number(stats.get('followers', 0))
-    views = format_number(stats.get('views', 0))
     likes = format_number(stats.get('likes', 0))
+    views = format_number(stats.get('views', 0))
+    creations = format_number(stats.get('creations', 0))
 
-    # 340px 宽度与 Stats 卡片一致，README 中通过 width="32%" 控制显示大小
+    # 340px 宽度与 Stats 卡片一致，README 中通过 width="33%" 控制显示大小
     width = 340
     height = 200
     stroke_width = 1
@@ -107,17 +108,23 @@ def generate_vlog_card(stats):
             </g>
             <g transform="translate(0,25.2)" width="14" height="14" fill="#bf91f3">
                 <path fill-rule="evenodd"
-                    d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z" />
+                    d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.75.75 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25z" />
             </g>
             <g transform="translate(0,50.4)" width="14" height="14" fill="#bf91f3">
                 <path fill-rule="evenodd"
-                    d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.75.75 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25z" />
-            </g><text x="21" y="14" style="fill: #38bdae; font-size: 14px;">followers:</text><text x="21" y="39.2"
-                style="fill: #38bdae; font-size: 14px;">Views:</text><text x="21" y="64.4"
-                style="fill: #38bdae; font-size: 14px;">Likes:</text><text x="100" y="14"
+                    d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z" />
+            </g>
+            <g transform="translate(0,75.6)" width="14" height="14" fill="#bf91f3">
+                <path fill-rule="evenodd"
+                    d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z" />
+            </g><text x="21" y="14" style="fill: #38bdae; font-size: 14px;">Followers:</text><text x="21" y="39.2"
+                style="fill: #38bdae; font-size: 14px;">Likes:</text><text x="21" y="64.4"
+                style="fill: #38bdae; font-size: 14px;">Views:</text><text x="21" y="89.6"
+                style="fill: #38bdae; font-size: 14px;">Creations:</text><text x="100" y="14"
                 style="fill: #38bdae; font-size: 14px;">{followers}</text><text x="100" y="39.2"
-                style="fill: #38bdae; font-size: 14px;">{views}</text><text x="100" y="64.4"
-                style="fill: #38bdae; font-size: 14px;">{likes}</text>
+                style="fill: #38bdae; font-size: 14px;">{likes}</text><text x="100" y="64.4"
+                style="fill: #38bdae; font-size: 14px;">{views}</text><text x="100" y="89.6"
+                style="fill: #38bdae; font-size: 14px;">{creations}</text>
         </g>
         <g transform="translate(235,25)">
             <g transform="scale(5)" style="fill: #bf91f3;">
