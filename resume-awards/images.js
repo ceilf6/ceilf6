b904bb6ce16e7be6b9e5baf718de5eaf1269c57b
WJH <3506456886@qq.com>
2026-03-05 13:44:08 +0800

    feat: 更新瀑布流实现

--- resume-awards/images.js
+++ resume-awards/images.js
@@ -1,14 +1,15 @@
 const images = [
   { src: "resume-awards/imgs/0004-字节工训营.JPG", alt: "字节工训营" },
-  { src: "resume-awards/imgs/0001-天梯赛.PNG", alt: "天梯赛" },
-  { src: "resume-awards/imgs/0002-睿抗.JPG", alt: "睿抗" },
+  { src: "resume-awards/imgs/腾讯青科实训营.PNG", alt: "腾讯青科实训营" },
   { src: "resume-awards/imgs/0003-腾讯开源.jpg", alt: "腾讯开源" },
   { src: "resume-awards/imgs/0010-百度之星.JPG", alt: "百度之星" },
-  { src: "resume-awards/imgs/0011-码蹄杯.JPG", alt: "码蹄杯" },
   {
     src: "resume-awards/imgs/0013-Datawhale-Agent.JPG",
     alt: "Datawhale-Agent",
   },
+  { src: "resume-awards/imgs/0001-天梯赛.PNG", alt: "天梯赛" },
+  { src: "resume-awards/imgs/0002-睿抗.JPG", alt: "睿抗" },
+  { src: "resume-awards/imgs/0011-码蹄杯.JPG", alt: "码蹄杯" },
   {
     src: "resume-awards/imgs/0101-美国大学生数学建模大赛.jpg",
     alt: "美国大学生数学建模大赛",
