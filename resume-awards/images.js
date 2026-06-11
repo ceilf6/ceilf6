e4ff80349fe8c5e7e84caad31bb866ff5b7514b2
WJH <3506456886@qq.com>
2026-06-11 18:02:31 +0800

    feat: 加快图片显示速度 1. 压缩体积 2. thumb 点开始仍然用原图 3. loading="lazy" + decoding="async"

--- resume-awards/images.js
+++ resume-awards/images.js
@@ -1,26 +1,78 @@
+// src: 原图（viewer.html 大图查看用）；thumb: 压缩缩略图（index.html 瀑布流用）
 const images = [
-  { src: "resume-awards/imgs/0004-字节工训营.JPG", alt: "字节工训营" },
-  { src: "resume-awards/imgs/腾讯青科实训营.png", alt: "腾讯青科实训营" },
-  { src: "resume-awards/imgs/0003-腾讯开源.jpg", alt: "腾讯开源" },
-  { src: "resume-awards/imgs/0010-百度之星.JPG", alt: "百度之星" },
+  {
+    src: "resume-awards/imgs/0004-字节工训营.JPG",
+    thumb: "resume-awards/imgs/thumbs/0004-字节工训营.jpg",
+    alt: "字节工训营",
+  },
+  {
+    src: "resume-awards/imgs/腾讯青科实训营.png",
+    thumb: "resume-awards/imgs/thumbs/腾讯青科实训营.jpg",
+    alt: "腾讯青科实训营",
+  },
+  {
+    src: "resume-awards/imgs/0003-腾讯开源.jpg",
+    thumb: "resume-awards/imgs/thumbs/0003-腾讯开源.jpg",
+    alt: "腾讯开源",
+  },
+  {
+    src: "resume-awards/imgs/0010-百度之星.JPG",
+    thumb: "resume-awards/imgs/thumbs/0010-百度之星.jpg",
+    alt: "百度之星",
+  },
   {
     src: "resume-awards/imgs/0013-Datawhale-Agent.JPG",
+    thumb: "resume-awards/imgs/thumbs/0013-Datawhale-Agent.jpg",
     alt: "Datawhale-Agent",
   },
-  { src: "resume-awards/imgs/0001-天梯赛.PNG", alt: "天梯赛" },
-  { src: "resume-awards/imgs/0002-睿抗.JPG", alt: "睿抗" },
-  { src: "resume-awards/imgs/0011-码蹄杯.JPG", alt: "码蹄杯" },
+  {
+    src: "resume-awards/imgs/0001-天梯赛.PNG",
+    thumb: "resume-awards/imgs/thumbs/0001-天梯赛.jpg",
+    alt: "天梯赛",
+  },
+  {
+    src: "resume-awards/imgs/0002-睿抗.JPG",
+    thumb: "resume-awards/imgs/thumbs/0002-睿抗.jpg",
+    alt: "睿抗",
+  },
+  {
+    src: "resume-awards/imgs/0011-码蹄杯.JPG",
+    thumb: "resume-awards/imgs/thumbs/0011-码蹄杯.jpg",
+    alt: "码蹄杯",
+  },
   {
     src: "resume-awards/imgs/0101-美国大学生数学建模大赛.jpg",
+    thumb: "resume-awards/imgs/thumbs/0101-美国大学生数学建模大赛.jpg",
     alt: "美国大学生数学建模大赛",
   },
-  { src: "resume-awards/imgs/1001-上大-优秀学生.jpg", alt: "优秀学生" },
-  { src: "resume-awards/imgs/1002-上大公益奖学金.JPG", alt: "公益奖学金" },
-  { src: "resume-awards/imgs/1003-上大科创奖学金.JPG", alt: "科创奖学金" },
-  { src: "resume-awards/imgs/1004-上大科创奖学金2.JPG", alt: "科创奖学金2" },
-  { src: "resume-awards/imgs/1005-企创大赛特等奖.jpg", alt: "企创大赛特等奖" },
+  {
+    src: "resume-awards/imgs/1001-上大-优秀学生.jpg",
+    thumb: "resume-awards/imgs/thumbs/1001-上大-优秀学生.jpg",
+    alt: "优秀学生",
+  },
+  {
+    src: "resume-awards/imgs/1002-上大公益奖学金.JPG",
+    thumb: "resume-awards/imgs/thumbs/1002-上大公益奖学金.jpg",
+    alt: "公益奖学金",
+  },
+  {
+    src: "resume-awards/imgs/1003-上大科创奖学金.JPG",
+    thumb: "resume-awards/imgs/thumbs/1003-上大科创奖学金.jpg",
+    alt: "科创奖学金",
+  },
+  {
+    src: "resume-awards/imgs/1004-上大科创奖学金2.JPG",
+    thumb: "resume-awards/imgs/thumbs/1004-上大科创奖学金2.jpg",
+    alt: "科创奖学金2",
+  },
+  {
+    src: "resume-awards/imgs/1005-企创大赛特等奖.jpg",
+    thumb: "resume-awards/imgs/thumbs/1005-企创大赛特等奖.jpg",
+    alt: "企创大赛特等奖",
+  },
   {
     src: "resume-awards/imgs/中国计算机协会CSP证书.png",
+    thumb: "resume-awards/imgs/thumbs/中国计算机协会CSP证书.jpg",
     alt: "中国计算机协会CSP证书",
   },
 ];
